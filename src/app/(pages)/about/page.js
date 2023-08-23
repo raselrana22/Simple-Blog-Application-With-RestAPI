cc3dfe46fe0f0390a57e0522d7652a313f9ecec5
Rasel Rana <rasel.learn22@gmail.com>
2023-08-24 02:14:49 +0600

    Make the about page responsive

--- src/app/(pages)/about/page.js
+++ src/app/(pages)/about/page.js
@@ -3,17 +3,18 @@ import AbutBlog from "@/app/components/aboutBlog";
 
 export default function About() {
     return (
-        <div className="flex p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-            <div className="w-1/2 bg-slate-200 shadow-lg mr-2 text-justify">
-                <div className="mr-2">
+        <div className="flex flex-col md:flex-row p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            <div className="w-full pr-2 md:w-1/2 bg-slate-200 shadow-lg mb-2 md:mb-0 md:mr-2 text-justify">
+                <div className="p-2">
                     <AboutPage />
                 </div>
             </div>
-            <div className="w-1/2 bg-slate-200 shadow-lg ml-1 text-justify">
-                <div>
+            <div className="w-full pr-2 md:w-1/2 bg-slate-200 shadow-lg text-justify">
+                <div className="p-2">
                     <AbutBlog />
                 </div>
             </div>
         </div>
     );
 }
+
