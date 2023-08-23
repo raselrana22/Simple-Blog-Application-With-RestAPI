bd66f50cbaba5f0baa0d3d23f7b793c8280e6bce
Rasel Rana <rasel.learn22@gmail.com>
2023-08-24 02:33:18 +0600

    add lib documentation

--- src/app/lib/utils/validateForm.js
+++ src/app/lib/utils/validateForm.js
@@ -1,4 +1,14 @@
 // formValidate.js
+/**
+ * Validates form data for the contact form.
+ *
+ * @param {Object} formData - The form data to be validated.
+ * @param {string} formData.name - The name input value.
+ * @param {string} formData.email - The email input value.
+ * @param {string} formData.website - The website input value.
+ * @param {string} formData.message - The message input value.
+ * @returns {Object} - An object containing validation errors.
+ */
 
 export default function validateForm(formData) {
     const errors = {};
